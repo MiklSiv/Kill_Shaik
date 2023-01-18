1c1bca6431d624fc9790249a5f7bafcb5dc6dcb2
Mikl <ma_sivkov@mail.ru>
2023-01-18 23:01:57 +0300

    Цикл чтения сигнала
    - функция считывает до появления сигнала "U"

--- prov_arduino.py
+++ prov_arduino.py
@@ -2,15 +2,33 @@ import time
 
 import serial
 
-text = "DFGH"
-
-ser = serial.Serial("COM3", timeout = 5)
-if ser.isOpen():
-    ser.write(text.encode())
-    print('write')
-    print(ser.readline().decode('utf-8'))
+text = "HELLO"
 
 
+def com_ty():
+    ser = serial.Serial("COM4", timeout=3)
+    if ser.isOpen():
+        ser.write(text.encode('utf-8'))
+        print('write')
+        print(ser.readline().decode('utf-8'))
+
+def com_er():
+    ser = serial.Serial("COM5", timeout=3)
+    count = 1
+    while True:
+
+        if ser.isOpen():
+            if count == 10:
+                break
+            elif ser.read().decode('utf-8') == 'U':
+               count += 1
+               print(f' {count}   {ser.read(size=5).decode()}' )
+            else:
+               pass
+
+
+
+com_er()
 
 
 
